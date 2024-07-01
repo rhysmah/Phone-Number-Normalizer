1c60893a4689ac89eebe9f65e10a845735b590f0
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-07-01 10:37:55 -0700

    refactor: streamline code, update docs

--- database/database.go
+++ database/database.go
@@ -38,8 +38,7 @@ func InitializeDB() error {
 // - nil if the connection is successfully closed.
 // - An error if there is an issue closing the database connection.
 func CloseDB() error {
-	err := db.Close()
-	if err != nil {
+	if err := db.Close(); err != nil {
 		return fmt.Errorf("error closing database: %w", err)
 	}
 
@@ -47,23 +46,25 @@ func CloseDB() error {
 }
 
 // CreateTable creates the "phoneNumbers" table in the SQLite database
-// if it does not already exist.
+// if it does not already exist and inserts initial data if the table is empty.
 //
 // The table schema includes:
 // - id: an INTEGER PRIMARY KEY with AUTOINCREMENT
 // - numbers: a TEXT field
 //
+// Parameters:
+// - numbers: A slice of strings containing the initial data to be inserted.
+//
 // Returns:
-// - nil if the table is successfully created or already exists.
-// - An error if there is an issue executing the table creation.
+// - nil if the table is successfully created or already exists and data is inserted if the table is empty.
+// - An error if there is an issue executing the table creation or inserting the initial data.
 func CreateTable(numbers []string) error {
 	createTable := `CREATE TABLE IF NOT EXISTS phoneNumbers (
 		id INTEGER PRIMARY KEY AUTOINCREMENT, 
 		numbers TEXT
 	);`
 
-	_, err := db.Exec(createTable)
-	if err != nil {
+	if _, err := db.Exec(createTable); err != nil {
 		return fmt.Errorf("error creating table: %w", err)
 	}
 
@@ -74,11 +75,19 @@ func CreateTable(numbers []string) error {
 	return nil
 }
 
+// insertInitialData inserts the initial data into the "phoneNumbers" table
+// if the table is empty.
+//
+// Parameters:
+// - numbers: A slice of strings containing the initial data to be inserted.
+//
+// Returns:
+// - nil if the initial data is successfully inserted or if the table is already populated.
+// - An error if there is an issue accessing the data or inserting the initial data.
 func insertInitialData(numbers []string) error {
 	var count int
 
-	err := db.QueryRow(`SELECT COUNT(*) from phoneNumbers`).Scan(&count)
-	if err != nil {
+	if err := db.QueryRow(`SELECT COUNT(*) from phoneNumbers`).Scan(&count); err != nil {
 		return fmt.Errorf("error accessing data in database: %w", err)
 	}
 
@@ -88,8 +97,7 @@ func insertInitialData(numbers []string) error {
 	}
 
 	for _, number := range numbers {
-		_, err := db.Exec(`INSERT INTO phoneNumbers (numbers) VALUES (?)`, number)
-		if err != nil {
+		if _, err := db.Exec(`INSERT INTO phoneNumbers (numbers) VALUES (?)`, number); err != nil {
 			return fmt.Errorf("error inserting number '%s' column in table: %w", number, err)
 		}
 	}
@@ -97,28 +105,30 @@ func insertInitialData(numbers []string) error {
 	return nil
 }
 
+// ReadNumbers reads all phone numbers from the "phoneNumbers" table.
+//
+// Returns:
+// - A slice of strings containing the phone numbers.
+// - An error if there is an issue reading the data.
 func ReadNumbers() ([]string, error) {
 	var numbers []string
 
-	q := "SELECT * FROM phoneNumbers"
-
-	// Extract rows that meet query
-	rows, err := db.Query(q)
+	rows, err := db.Query("SELECT * FROM phoneNumbers")
 	if err != nil {
 		return nil, fmt.Errorf("error reading data: %w", err)
 	}
 	defer rows.Close()
 
-	// Scan each row, read numbers, add them to string slice
 	for rows.Next() {
-
-		var id int64
 		var num string
-
-		if err := rows.Scan(&id, &num); err != nil {
+		if err := rows.Scan(&num); err != nil {
 			return nil, fmt.Errorf("error reading rows: %w", err)
 		}
 		numbers = append(numbers, num)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("error iterating rows: %w", err)
 	}
 
 	return numbers, nil
